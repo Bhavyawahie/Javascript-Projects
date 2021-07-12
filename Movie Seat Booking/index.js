303fbda58b2cf16c06377519ffbb9993212d880e
Bhavya Wahie <bhavyawahie@gmail.com>
2021-07-13 02:45:06 +0530

    Fetch functionality from localStorage Added

--- Movie Seat Booking/index.js
+++ Movie Seat Booking/index.js
@@ -4,19 +4,50 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUi();
+
 let ticketPrice = parseInt(movieSelect.value);
 
+
+const saveMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem("selectedMovieIndex", movieIndex)
+    localStorage.setItem("selectedMoviePrice", moviePrice)
+}
+
 const updateSelectedCount = () => {
-    const selectedSeats = document.querySelectorAll(".rows .seat.selected")
+    const selectedSeats = document.querySelectorAll(".rows .seat.selected");
+
+    const seatsIndex = [...selectedSeats].map((seat) => {
+        return [...seats].indexOf(seat)
+    });
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
     
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+function populateUi()  {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1){
+                seat.classList.add("selected");
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Movie Selector event listener
 movieSelect.addEventListener("change", (e) => {
     ticketPrice = parseInt(e.target.value);
+    saveMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount();
 })
 
@@ -28,3 +59,5 @@ container.addEventListener("click", (e) => {
         updateSelectedCount();
     }
 })
+// Initial count and total set
+updateSelectedCount();
